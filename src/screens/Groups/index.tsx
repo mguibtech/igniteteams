f2efd7ad3dbaecbc96012cab991dfccdf7d905e1
mguib <mguibtech@gmail.com>
2022-12-28 09:23:43 -0400

    Creating component the list groups with a flatList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,14 @@
-
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highight";
 import { Container } from "./styles";
 
 export function Groups() {
+
+    const [groups, setGroups] = useState<string[]>(['Galera do Valorant', 'Galera do LOL', 'Amigos', 'Familia']);
+
     return (
         <Container>
             <Header />
@@ -12,7 +16,17 @@ export function Groups() {
                 title="Turmas"
                 subtitle="Jogue com a sua turma!"
             />
-            <GroupCard title="Galera do Ignite"/>
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+            />
+
         </Container>
-    )
+    );
 }
