cbab69593be5d77695fd553127f001c2aaa932a4
mguib <mguibtech@gmail.com>
2022-12-28 12:08:33 -0400

    Modifying theme imports using css Helper

--- src/components/GroupCard/styles.ts
+++ src/components/GroupCard/styles.ts
@@ -1,12 +1,12 @@
 import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
-import {UsersThree} from 'phosphor-react-native'
+import styled, { css } from "styled-components/native";
+import { UsersThree } from 'phosphor-react-native'
 
 export const Container = styled(TouchableOpacity)`
   width: 100%;
   height: 90px;
 
-  background-color: ${({theme}) => theme.COLORS.GRAY_500};
+  background-color: ${({ theme }) => theme.COLORS.GRAY_500};
   border-radius: 6px;
 
   flex-direction: row;
@@ -17,12 +17,14 @@ export const Container = styled(TouchableOpacity)`
 `;
 
 export const Title = styled.Text`
-    font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
-    color: ${({theme}) => theme.COLORS.GRAY_200};
-    font-family: ${({theme}) => theme.FONT_FAMILY.REGULAR};
+    ${({ theme }) => css`
+        font-size: ${theme.FONT_SIZE.MD}px;
+        color: ${theme.COLORS.GRAY_200};
+        font-family: ${theme.FONT_FAMILY.REGULAR};
+    `}    
 `;
 
-export const Icon = styled(UsersThree).attrs(({theme})=> ({
+export const Icon = styled(UsersThree).attrs(({ theme }) => ({
     size: 32,
     color: theme.COLORS.GREEN_700,
     weight: 'fill'
