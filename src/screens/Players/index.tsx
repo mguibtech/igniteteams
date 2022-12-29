02b6b96133029c6b555b1d9f7b1683272c8449a0
mguib <mguibtech@gmail.com>
2022-12-29 14:08:00 -0400

    Creating team filter styles

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,13 +1,19 @@
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Fillter } from "@components/Fillter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highight";
 import { Input } from "@components/Input";
-import { Container, Form } from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
-export function Players(){
-    return(
+export function Players() {
+    const [team, setTeam] = useState('Time A')
+    const [players, setPlayers] = useState(['Guibson'])
+
+    return (
         <Container>
-            <Header showBackButton/>
+            <Header showBackButton />
 
             <Highlight
                 title="Nome da Turma"
@@ -19,9 +25,27 @@ export function Players(){
                     placeholder="Nome da Pessoa"
                     autoCorrect={false}
                 />
-                <ButtonIcon icon="add"/>
+                <ButtonIcon icon="add" />
             </Form>
-            
+
+            <HeaderList>
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                    <Fillter
+                        title={item}
+                        isActive = {item === team}
+                        onPress={() => setTeam(item)}
+                    />)}
+                    horizontal
+                />
+
+                <NumberOfPlayers>
+                    {players.length}
+                </NumberOfPlayers>
+            </HeaderList>
+
             
         </Container>
     )
