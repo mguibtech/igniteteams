87d4d80553a6a6382a884f58ccbafb2783daa434
mguib <mguibtech@gmail.com>
2023-01-10 21:57:40 -0400

    creating glitch prevention in navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { View } from 'react-native';
 import { AppRoutes } from './app.routes';
+import { useTheme } from 'styled-components/native';   
 
-export function Routes(){
-    return(
-        <NavigationContainer>
-            <AppRoutes/>
-        </NavigationContainer>
+export function Routes() {
+
+    const {COLORS} = useTheme()
+    return (
+        <View style={{flex:1, backgroundColor: COLORS.GRAY_600}}>
+            <NavigationContainer>
+                <AppRoutes />
+            </NavigationContainer>
+        </View>
     )
 }
